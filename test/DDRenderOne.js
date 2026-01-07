2603cec8235300f67b603bb09cbb06948c055bbf
orstavik <orstavik77@gmail.com>
2026-01-07 20:15:10 +0100

    working

--- test/DDRenderOne.js
+++ test/DDRenderOne.js
@@ -4,25 +4,40 @@ function makeDocFrag(str) {
   return tmp.content.cloneNode(true);
 }
 
-function resolvePath(root, path) {
-  return path.reduce((n, i) =>
-    typeof i == "string" ? n.getAttributeNode(i) :
-      n.childNodes[i],
-    root);
-}
+const reducePath = (root, path) => path.reduce((n, i) =>
+  typeof i == "string" ? n.getAttributeNode(i) : n.childNodes[i], root);
 
 export function RenderOne(state, start, end, DollarDotsDef) {
   const __state = Object.assign({}, state);
-  while (start.nextSibling != end)
-    start.nextSibling.remove();
+  const newNodes = [];
   DollarDotsDef.hydra(__state, function run() {
     const root = makeDocFrag(DollarDotsDef.templateString);
     for (let { path, id, hydra, stateReferences } of DollarDotsDef.innerHydras) {
-      const n = resolvePath(root, path);
+      const n = reducePath(root, path);
       id ?
         RenderOne(__state, n, n.nextSibling, window.dollarDots[id]) :
         n.nodeValue = hydra(__state);
     }
+    newNodes.push(...root.childNodes);
+  });
+  while (start.nextSibling != end)
+    start.nextSibling.remove();
+  start.after(...newNodes);
+}
+
+export function RenderTwo(state, start, end, DollarDotsDef) {
+  const __state = Object.assign({}, state);
+  DollarDotsDef.hydra(__state, function run() {
+    const root = makeDocFrag(DollarDotsDef.templateString);
+    for (let { path, id, hydra, stateReferences } of DollarDotsDef.innerHydras) {
+      const n = reducePath(root, path);
+      id ?
+        RenderTwo(__state, n, n.nextSibling, window.dollarDots[id]) :
+        n.nodeValue = hydra(__state);
+    }
+    while (start.nextSibling != end)
+      start.nextSibling.remove();
     start.after(...root.childNodes);
   });
 }
+
