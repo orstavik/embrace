25c9786bc1e08fafc631460fb74646c1da485801
orstavik <orstavik77@gmail.com>
2026-01-11 15:02:32 +0100

    bs

--- test/DD.js
+++ test/DD.js
@@ -1,32 +1,30 @@
 window.dollarDots = {};
 
+const resolvePath = (root, path) => path.reduce((n, i) =>
+  typeof i == "string" ? n.getAttributeNode(i) : n.childNodes[i], root);
+
 export function register(template) {
   window.dollarDots[template.id] = template;
-  //todo we should not be able to register a template that has a broken reference to an inner Def..
-  //todo that meanst that it is ok to put the Def in the register here, not 
-}
-
-export function makeDocFrag(str) {
-  const tmp = document.createElement("template");
-  tmp.innerHTML = str;
-  return tmp.content.cloneNode(true);
+  const el = document.createElement("template");
+  el.innerHTML = template.templateString;
+  template.docFrag = el.content;
+  delete template.templateString;
+  template.innerHydras = template.innerHydras.map(({ id, path, hydra }) => ({
+    id,
+    path,
+    hydra,
+    Def: getDefinition(id),
+  }));
 }
 
 export function getDefinition(id) {
   return window.dollarDots[id];
 }
 
-export const resolvePath = (root, path) => path.reduce((n, i) =>
-  typeof i == "string" ? n.getAttributeNode(i) : n.childNodes[i], root);
-
-
 export function getInstance(Def) {
-  const root = makeDocFrag(Def.templateString);
-  const innerHydras = Def.innerHydras.map(({ id, path, hydra }) => ({
-    hydra,
-    node: resolvePath(root, path),
-    Def: getDefinition(id),
-  }));
+  const root = Def.docFrag.cloneNode(true);
+  const innerHydras = Def.innerHydras.map(({ Def, path, hydra }) =>
+    ({ Def, hydra, node: resolvePath(root, path) }));
   return { root, innerHydras };
 }
 
