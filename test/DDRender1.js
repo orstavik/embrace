59b09470d1430abb681d416e44ecd76f91509fd5
orstavik <orstavik77@gmail.com>
2026-01-11 14:48:14 +0100

    bs

--- test/DDRender1.js
+++ test/DDRender1.js
@@ -1,19 +1,23 @@
 import { getDefinition, findRunnableTemplates, getInstance } from "./DD.js";
 
+function replaceNodesBetween(start, end, ...nodes) {
+  while (start.nextSibling != end)
+    start.nextSibling.remove();
+  start.after(...nodes);
+}
+
 function render(state, start, end, DollarDotsDef) {
   const __state = Object.assign({}, state);
   const newNodes = [];
   DollarDotsDef.hydra(__state, function run() {
     const { root, innerHydras } = getInstance(DollarDotsDef.id);
-    for (let { node: n, Def, hydra } of innerHydras)
+    for (let { node, hydra, Def } of innerHydras)
       Def ?
-        render(__state, n, n.nextSibling, Def) :
-        n.nodeValue = hydra(__state);
+        render(__state, node, node.nextSibling, Def) :
+        node.nodeValue = hydra(__state);
     newNodes.push(...root.childNodes);
   });
-  while (start.nextSibling != end)
-    start.nextSibling.remove();
-  start.after(...newNodes);
+  replaceNodesBetween(start, end, ...newNodes);
 }
 
 export function renderUnder(root, state) {
