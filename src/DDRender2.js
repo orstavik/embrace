801b616d43515243d5303bfb700c6f93ab3fd612
orstavik <orstavik77@gmail.com>
2026-01-13 10:51:15 +0100

    bs

--- src/DDRender2.js
+++ src/DDRender2.js
@@ -42,8 +42,6 @@ function render(state, start, end, Def, rootCtx) {
         node.nodeValue = hydra($);
     for (let n of nodes)
       newNodes.push(rootCtx.tryToReuse(n) ?? n);
-    //todo if tryToReuse is successful, then we can reuse the getInstance(). 
-    //But then we need to remove everything between startEnd pairs.
   });
   rootCtx.addNewNodes(newNodes);
   start.after(...newNodes);
@@ -59,9 +57,57 @@ function startUp(root) {
   return ctx;
 }
 
+function SelectionRestorer(active) {
+  const okInputTypes = /text|search|password|tel|url|email/i;
+  if (active.tagName == "TEXTAREA" || (active.tagName == "INPUT" && okInputTypes.test(active.type))) {
+    const { selectionStart, selectionEnd, selectionDirection } = active;
+    if (selectionStart != null)
+      return active => active.setSelectionRange(selectionStart, selectionEnd, selectionDirection);
+  }
+}
+
+function FocusSelectionRestorer(root) {
+  const { id, tagName, type, name, value } = document.activeElement;
+  const selection = SelectionRestorer(document.activeElement);
+
+  if (id)
+    return () => {
+      const active = root.querySelector(`#${CSS.escape(id)}`);
+      if (!active)
+        return;
+      active.focus();
+      selection?.(active);
+    };
+
+  let q = tagName;
+  if (/input|textarea|select/i.test(tagName)) {
+    if (name) q += `[name="${CSS.escape(name)}"]`;
+    if (type) q += `[type="${CSS.escape(type)}"]`;
+  } else {
+    const contenteditable = document.activeElement.getAttribute("contenteditable");
+    const tabindex = document.activeElement.getAttribute("tabindex");
+    if (contenteditable) q += `[contenteditable="${CSS.escape(contenteditable)}"]`;
+    if (tabindex) q += `[tabindex="${CSS.escape(tabindex)}"]`;
+  }
+
+  const equalInputs = [...root.querySelectorAll(q)]
+    .filter(n => n.value === value)
+    .indexOf(document.activeElement);
+
+  return function () {
+    const active = [...root.querySelectorAll(q)].filter(n => n.value === value)[equalInputs];
+    if (!active)
+      return;
+    active.focus();
+    selection?.(active);
+  };
+}
+
 export function renderUnder(root, state) {
+  const focusRestorer = root.contains(document.activeElement) && FocusSelectionRestorer(root);
   const rootCtx = startUp(root);
   for (let { start, id, end } of findRunnableTemplates(root))
     render(state, start, end, getDefinition(id), rootCtx);
   rootCtx.cleanUp();
+  !root.contains(document.activeElement) && focusRestorer?.();
 }
