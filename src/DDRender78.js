50d73c6d7409ceb7942a864813ef98aaeea0bebd
orstavik <orstavik77@gmail.com>
2026-01-15 12:35:30 +0100

    bs

--- src/DDRender78.js
+++ src/DDRender78.js
@@ -2,22 +2,23 @@ import { getDefinition, findRunnableTemplates, getInstance } from "./DD7.js";
 import { FocusSelectionRestorer } from "./DDFocusRestorer.js";
 
 //att!! assumes that arrayWithValues has already been unified.
+//Def => {valuesArrayOfArraysAsKey => [...reusableInstances]}
+//Def => {valuesArrayAsKey => [...reusableInstances]}
 class ReuseMap {
 
-  //Def => {valuesArrayOfArraysAsKey => [...reusableInstances]}
-  //Def => {valuesArrayAsKey => [...reusableInstances]}
-  #prevMap = new Map();
-  #nextMap = new Map();
+  #map = new Map();
 
-  flip() {
-    this.#prevMap = this.#nextMap;
-    this.#nextMap = new Map();
+  putAll(a, b, v) {
+    this.put(a, b, v);
+    for (let i = 0; i < v.instances.length; i++)
+      if (v.instances[i])
+        this.put(v.Def, v.values[i], v.instances[i]);
   }
 
   put(a, b, v) {
-    let group = this.#nextMap.get(a);
+    let group = this.#map.get(a);
     if (!group)
-      this.#nextMap.set(a, group = new Map());
+      this.#map.set(a, group = new Map());
     let set = group.get(b);
     if (!set)
       group.set(b, set = new Set());
@@ -25,27 +26,25 @@ class ReuseMap {
   }
 
   reuseMatch(a, b, matcher) {
-    const set = this.#prevMap.get(a)?.get(b);
+    const set = this.#map.get(a)?.get(b);
     if (set)
       for (let v of set)
         if (matcher(v)) {
           set.delete(v);
-          this.put(a, b, v);
           return v;
         }
   }
 
   reuseAny(a, b) {
-    const set = this.#prevMap.get(a)?.get(b);
+    const set = this.#map.get(a)?.get(b);
     if (!set)
       return;
     set.delete(b);
-    this.put(a, b, set);
     return set;
   }
 
   pop(a) {
-    const topMap = this.#prevMap.get(a);
+    const topMap = this.#map.get(a);
     if (!topMap)
       return;
     const next = topMap.entries().next();
@@ -55,65 +54,67 @@ class ReuseMap {
     topMap.delete(k);
     return v;
   }
-
-  breakUpValuesIntoPieces(a) {
-    const topMap = this.#prevMap.get(a);
-    if (!topMap)
-      return;
-    debugger
-    const values = topMap.values();
-    // const allTheGroupsSet = new Set([...topMap.values()]);
-    //1.  
-  }
 }
 
 const tupleMap = {};
 const tuplify = (obj) => tupleMap[JSON.stringify(obj)] ??= obj;
 
-function renderValues(state, Def, intro = "") {
+function renderValues(state, Def) {
   const $ = Object.assign(state instanceof Array ? [] : {}, state);
   const values = [];
   Def.hydra($, function run() {
-    const innerValues = Def.innerHydras.map(({ Def: D2, hydra }) =>
-      D2 ? renderValues($, D2, intro + "|" + Def.id) : hydra($));
-    values.push(tuplify(innerValues));
+    const innerValues = Def.innerHydras.map(inner =>
+      inner.Def ? renderValues($, inner.Def) : inner.hydra($));
+    values.push(innerValues);
   });
-  return { values: tuplify(values), intro };
-  //todo if i can get the intro out in a separate channel, then we are much better positioned.
+  return tuplify(values.map(tuplify)); //tuplify self and direct children.
 }
 
-function reuse(todo, reusables) {
+function xIsDescendantOf(xDef, otherDef) {
+  for (let inner of otherDef.innerHydras)
+    if (inner.Def && (inner.Def === xDef || xIsDescendantOf(xDef, inner.Def)))
+      return true;
+}
+function firstDefNotADescendant(tasks) {
+  return [...new Set(tasks.map(({ Def }) => Def))]
+    .find(def => tasks.every(({ Def }) => !xIsDescendantOf(def, Def)));
+}
+
+const reusables = new ReuseMap();
+function reuse(todo) {
+  const nextReusables = new ReuseMap();
   const removeables = [];
   while (todo.length) {
-    //extract the first topLevel task Def, that is a Def that is NEVER inside another Def.
-    const topDef = todo.find(task => todo.every(t2 => !t2.intro.includes(task.Def.id))).Def;
-    //get all the tasks with this DefType. We run all DefTypes in one go.
+    debugger
+    const topDef = firstDefNotADescendant(todo);
     const nowTasks = todo.filter(({ Def }) => Def === topDef);
     todo = todo.filter(t => !nowTasks.includes(t));
 
-    debugger
     //1. superExact reusables: identical values and identical .start comment, 
     //   ie. no replacement necessary as the reusable is already in the right place.
-    const nowTasks2 = nowTasks.filter(nowTask =>
-      !reusables.reuseMatch(topDef, nowTask.values, reTask => reTask.start === nowTask.start));
+    const nowTasks2 = [];
+    for (let nowTask of nowTasks) {
+      const task = reusables.reuseMatch(topDef, nowTask.values, reTask => reTask.start === nowTask.start);
+      task ?
+        nextReusables.putAll(topDef, nowTask.values, task) :
+        nowTasks2.push(nowTask);
+    }
 
     //2. exact value match: identical values, but positioned elsewhere in the DOM.
     //   we must move the reusable to the new position.
     const nowTasks3 = [];
     for (let nowTask of nowTasks2) {
       const reuseTask = reusables.reuseAny(topDef, nowTask.values);
-      reuseTask ?
-        inject(nowTask.start, reuseTask.start.nextSibling, reuseTask.end.previousSibling) :
+      if (reuseTask) {
+        nextReusables.putAll(topDef, nowTask.values, reuseTask);
+        inject(nowTask.start, reuseTask.start.nextSibling, reuseTask.end.previousSibling);
+      } else {
         nowTasks3.push(nowTask);
+      }
     }
 
-    //3. we can do a partial reuse. Here we need to break up the values array into 
-    //   this is a specialized operation. We need to take the values array and break it up into chunks.
-    //   this essentially needs to spool the childNodes and find the `::,` comments.
-    //   
-    reusables.breakUpValuesIntoPieces(topDef);
-
-    //1. prep nowTask comments and instances arrays
+    //3. Reuse run() instances
+    //A. prep nowTask with `<!--::,-->` comments and instances arrays
     for (let nowTask of nowTasks3) {
       nowTask.comments = [
         nowTask.start,
@@ -123,8 +124,7 @@ function reuse(todo, reusables) {
       nowTask.instances = Array(nowTask.values.length);
     }
 
-    //2. reuse exact
-    debugger
+    //B. reuse exact run() instance
     for (let nowTask of nowTasks3) {
       for (let i = 0; i < nowTask.values.length; i++) {
         const value = nowTask.values[i];
@@ -137,14 +137,11 @@ function reuse(todo, reusables) {
           end.before(reuseExact.end.previousSibling);
           reuseExact.start = start;
           reuseExact.end = end;
-          //todo It feels unsafe to have the reuseExact remember the new task. 
-          //     I think it should be different.
-          //     It feels like the nowTask should be added to the reusables.
         }
       }
     }
 
-    //3. reuse partial
+    //C. reuse partial
     main: for (let nowTask of nowTasks3) {
       for (let i = 0; i < nowTask.values.length; i++) {
         if (nowTask.instances[i]) continue;
@@ -160,7 +157,7 @@ function reuse(todo, reusables) {
           const { Def, node } = reusePartial.nodes[k];
           const innerValue = value[k];
           if (Def) {
-            todo.push({ Def, value: innerValue, start: nowTask.comments[i], end: nowTask.comments[i + 1] });
+            todo.push({ Def, values: innerValue, start: nowTask.comments[i], end: nowTask.comments[i + 1] });
           } else if (innerValue != node.nodeValue) {
             node.nodeValue = innerValue;
           }
@@ -168,7 +165,7 @@ function reuse(todo, reusables) {
       }
     }
 
-    //4. create new
+    //D. create new
     for (let nowTask of nowTasks3) {
       for (let i = 0; i < nowTask.values.length; i++) {
         if (nowTask.instances[i]) continue;
@@ -184,34 +181,34 @@ function reuse(todo, reusables) {
           const { Def, node } = newInstance.innerHydras[k];
           const innerValue = nowTask.values[i][k];
           if (Def) {
-            todo.push({ Def, values: innerValue.values, start, end });
+            todo.push({ Def, values: innerValue, start, end });
           } else {
             node.nodeValue = innerValue;
           }
         }
       }
     }
+    //4. add to reusables the tasks that needed internal handling
+    for (let nowTask of nowTasks3)
+      nextReusables.putAll(topDef, nowTask.values, nowTask);
   }
-  return removeables;
-}
-
-const reusables = new ReuseMap();
-export function renderUnder(root, state) {
-  const restoreFocus = root.contains(document.activeElement) && FocusSelectionRestorer(root);
-
-  const valueInstances = [];
-  for (let t of findRunnableTemplates(root)) {
-    t.Def = getDefinition(t.id);
-    const dvp = renderValues(state, t.Def);
-    Object.assign(dvp, t);
-    valueInstances.push(dvp);
-  }
-
-  reusables.flip();
-  const removeables = reuse(valueInstances, reusables);
   for (let [last, end] of removeables)
     for (let next = last.nextSibling; next != end; next = next.nextSibling)
       next.remove();
+}
+
+export function renderUnder(root, state) {
+  const restoreFocus = root.contains(document.activeElement) && FocusSelectionRestorer(root);
+
+  const tasks = [];
+  for (let task of findRunnableTemplates(root)) {
+    task.Def = getDefinition(task.id);
+    delete task.id;
+    task.values = renderValues(state, task.Def);
+    tasks.push(task);
+  }
+
+  reuse(tasks);
 
   restoreFocus && !root.contains(document.activeElement) && restoreFocus();
 }
