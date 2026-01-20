b691f8b21746084bac77ea7dedd6c5a6af0b14c0
orstavik <orstavik77@gmail.com>
2026-01-20 11:52:16 +0100

    bs

--- src/DDRender.js
+++ src/DDRender.js
@@ -206,9 +206,8 @@ const IdenticalInnerArrays = (f, r) => f.value.every((v, i) => typeof v === 'str
 //todo 1: make a test for the extractUnusedInnerReusables(Def). This will require reuse of common template ids.
 //        this is actually a little tricky. We can reuse the same dom if the insides of the templates are identical, 
 //        regardless of the if and for. This means that we actually would like the start and end nodes not be part of the same system.
-//todo 2: make the simple Renderer use DD6 and DDCompile6.
-//todo 3: fix the getInstance function so it is also a Stamp. That way we can hide #nodes and #start in the Stamp.
-//todo 4: remove the start and end node from the template type again?
+//todo 2: fix the getInstance function so it is also a Stamp. That way we can hide #nodes and #start in the Stamp.
+//todo 3: remove the start and end node from the template type again?
 
 function reuseAndInstantiateIndividualStamps(todos) {
   let globalNotUsed = new UnusedStampsMap();
